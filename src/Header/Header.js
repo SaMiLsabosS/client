6b25de149c78bd01522d55152539c79e944cf3b6
Sami Melhem <SamiISaboss23@gmail.com>
2023-01-28 21:21:35 -0600

    The Financial Ag

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 function Header() {
     return (
     <div className="header">
-          <h1>TFA</h1>
+          <h1>The Financial Ag</h1>
           <div className='rightButtons'>
             <button>
                 <h1>Home</h1>
