c0a5f5c2ceb529563e9d29e42c528bb28d681687
Chandler Ngo <chandlerngo00@gmail.com>
2023-01-28 21:37:08 -0600

    alt added

--- src/Articles/Article/Article.js
+++ src/Articles/Article/Article.js
@@ -4,7 +4,7 @@ function Article(props) {
     const place_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
     return (
     <div className="article">
-        <img src={props.image}></img>
+        <img src={props.image} alt={props.image}></img>
         <h1>{props.title}</h1>
         <p className='text'>{place_text}</p>
         <p>Learn more</p>
